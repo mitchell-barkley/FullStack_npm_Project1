cb5885a3660b15e8f46053a335e1c9b8ad3e8663
mitchell-barkley <138683597+mitchell-barkley@users.noreply.github.com>
2024-01-18 12:08:11 -0330

    http.js

--- validator.js
+++ validator.js
@@ -2,3 +2,5 @@ var validator = require('validator');
 
 console.log(validator.isEmail('foo@bar.org'));
 console.log(validator.isISBN('978-3-16-148410-0'));
+console.log(validator.isMobilePhone('15197347119', 'en-US'));
+console.log(validator.isURL('http://www.google.com'));
